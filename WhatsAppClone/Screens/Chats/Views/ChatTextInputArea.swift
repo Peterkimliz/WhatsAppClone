30f829e24895dcc1659b691981df1143360e43fa
Peterkimliz <p.kironji@yahoo.com>
2026-06-08 00:06:01 -0700

    latest up[dates

---
+++ WhatsAppClone/Screens/Chats/Views/ChatTextInputArea.swift
@@ -0,0 +1,18 @@
+//
+//  ChatTextInputArea.swift
+//  WhatsAppClone
+//
+//  Created by Apple on 07/06/2026.
+//
+
+import SwiftUI
+
+struct ChatTextInputArea: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatTextInputArea()
+}
